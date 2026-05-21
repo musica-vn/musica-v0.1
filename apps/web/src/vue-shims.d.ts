9c09235d88e7da0f433718d9244750451f0adb25
hyeu <nguyentrunghieuthptxp@gmail.com>
2026-05-21 09:36:23 +0700

    fix render build config

--- apps/web/src/vue-shims.d.ts
+++ apps/web/src/vue-shims.d.ts
@@ -2,3 +2,18 @@ declare module '*.vue' {
   const component: any
   export default component
 }
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.css' {
+  const css: unknown
+  export default css
+}
