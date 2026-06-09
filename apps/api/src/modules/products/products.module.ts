49f93bf1cf1ff3a3b46853edef901a86b02ee24a
theminhdayma <theminh2005z@gmail.com>
2026-06-09 21:11:47 +0700

    Update API marketplace

--- apps/api/src/modules/products/products.module.ts
+++ apps/api/src/modules/products/products.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SupabaseModule } from '../../database/supabase.module';
 import { AdminProductsController } from './admin-products.controller';
+import { MarketplaceProductsController } from './marketplace-products.controller';
 import { PublicProductsController } from './public-products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
   imports: [SupabaseModule],
-  controllers: [AdminProductsController, PublicProductsController],
+  controllers: [
+    AdminProductsController,
+    MarketplaceProductsController,
+    PublicProductsController,
+  ],
   providers: [ProductsService],
   exports: [ProductsService],
 })
